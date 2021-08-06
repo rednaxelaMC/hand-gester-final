2703b027c2ed14d9c481b5456e7e57510f954be0
rednaxelaMC <76746691+rednaxelaMC@users.noreply.github.com>
2021-08-06 12:50:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var perdiction_1 = "";
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera")
+
+Webcam.attach( '#camera' );
+
+function take_snapshot(){
+
+Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+});
+console.log("ml5 version", ml5.version);
+classifier= ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Dodmphcla/model.json', modelLoaded);
+function modelLoaded () {
+    console.log("modelLoaded");
+}
+}
+function check(){
+
+    img = document.getElementById("captured_image");
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+if (error){
+    console.error(error);
+}
+else {
+console.log(results);
+document.getElementById("result_emotion_name").innerHTML= results[0].label;
+document.getElementById("result_emotion_name2").innerHTML= results[1].label;
+perdiction_1 = results[0].label;
+perdiction_2 = results[1].label;
+speak();
+}
+}
+function speak(){
+var synth = window.speechSynthesis;
+speak_data1 = "the first perdiction is "+perdiction_1;
+speak_data2 = "and the second perdiction is "+perdiction_2;
+var utterThis = new SpeechSynthesisUtterance(speak_data1 + speak_data2);
+ synth.speak(utterThis);
+}
